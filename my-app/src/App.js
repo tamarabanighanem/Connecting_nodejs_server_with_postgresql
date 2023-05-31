25a1785c12dd3e22d430dd850c0aaf99f1d591df
tamarabanighanem <tbanighanem@gmail.com>
2023-05-31 01:03:57 -0700

    done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -150,33 +150,17 @@ function App() {
 
     <form onSubmit={handelsubmit} action="" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
       <div>
-        <label htmlFor="email" className="sr-only">UserName</label>
 
         <div className="relative">
-          <input
+          <label className='text-gray-400'> UserName<input
             type="text"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            placeholder="Enter UserName"
+            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-lg"
+            placeholder="Your Age"
             value={name}
             onChange={(e)=>setname(e.target.value)}
           />
-
-          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-              />
-            </svg>
-          </span>
+</label>
+        
         </div>
       </div>
 
@@ -184,15 +168,15 @@ function App() {
         <label htmlFor="password" className="sr-only">Age</label>
 
         <div className="relative">
-          <input
+        <label className='text-gray-400'>  <input
             type="number"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
-            placeholder="Enter Age"
+            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-lg"
+            placeholder="Your Age"
             value={age}
             onChange={(e)=>setage(e.target.value)}
           />
-
-          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+</label>
+          {/* <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-4 w-4 text-gray-400"
@@ -213,7 +197,7 @@ function App() {
                 d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
               />
             </svg>
-          </span>
+          </span> */}
         </div>
       </div>
 
